88436d8c50a57e9a849eb12d72f6e6d9b32ec37e
Colin Power <cp1372@mun.ca>
2016-04-05 09:15:50 -0230

    Preparing source files for OpenCV framework to be dragged in for demo

--- Surveillance/CameraViewController.mm
+++ Surveillance/CameraViewController.mm
@@ -1,48 +1,18 @@
 //
-//  MapViewController.m
-//  Surveillance
+//  CameraViewController.h
+//  OpenCV Test
 //
-//  Created by Dillon Butt on 2016-04-02.
-//  Copyright © 2016 Dillon Butt. All rights reserved.
+//  Created by Colin Power on 2016-04-02.
+//  Copyright © 2016 Colin Power. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "CameraViewController.h"
-#import "AppDelegate.h"
 
-@implementation CameraViewController
+#import <UIKit/UIKit.h>
 
--(void) viewDidLoad
+
+@interface CameraViewController : UIViewController <CvVideoCameraDelegate>
 {
     
-    //sets the tab bar badge value
-    self.tabBarItem.badgeValue = @"1";
-    
-    //use this one liner to set ANY of the badge values on the parent controller's tab bar.
-    [[[[[self tabBarController] tabBar] items] objectAtIndex:2] setBadgeValue:@"test"];    }
-
-
-- (void)viewDidAppear:(BOOL)animated
-{
-        //sets the current time on a label
-        NSDate * now = [NSDate date];
-        NSDateFormatter *outputFormatter = [[NSDateFormatter alloc] init];
-         //[outputFormatter setDateFormat:@"HH:mm:ss"];
-        [outputFormatter setDateFormat:@"HH':'mm':'ss' 'dd'/'MM'/'yyyy'"];
-        NSString *newDateString = [outputFormatter stringFromDate:now];
-        NSLog(@"Timestamp: %@", newDateString);
-        _timeStampLabel.text = newDateString;
-    
-    AppDelegate* test = [UIApplication sharedApplication].delegate;
-    [test.tableData addObject:newDateString];
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#
 
 @end
---
+++ Surveillance/CameraViewControllerNo.h
@@ -0,0 +1,18 @@
+////
+////  CameraViewController.h
+////  Surveillance
+////
+////  Created by Dillon Butt on 2016-04-02.
+////  Copyright © 2016 Dillon Butt. All rights reserved.
+////
+//
+//#ifndef CameraViewController_h
+//#define CameraViewController_h
+//#import <UIKit/UIKit.h>
+//
+//@interface CameraViewController : UIViewController
+//@property (strong, nonatomic) IBOutlet UILabel *timeStampLabel;
+//
+//@end
+//
+//#endif /* CameraViewController_h */
