ebdd75aec711aa33f8d7333e1a2755e95f8e07e2
Colin Power <cp1372@mun.ca>
2016-04-09 10:42:14 -0230

    Test 2

--- Surveillance/CameraViewController.mm
+++ Surveillance/CameraViewController.mm
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Colin Power. All rights reserved.
 //
 
-//test comment to test source control
+//test comment to test source control 2
 #include <opencv2/opencv.hpp>
 //using namespace cv;
 #import <opencv2/videoio/cap_ios.h>
