04ab8f80c89ea7bf19d79c9f88f11d5eb17e5103
Dillon <dillonbutt@gmail.com>
2016-04-12 18:02:19 -0230

    Opened event happened to public

--- Surveillance/MapViewController.h
+++ Surveillance/MapViewController.h
@@ -1,26 +1,22 @@
-//
-//  MapViewController.h
-//  Surveillance
-//
-//  Created by Dillon Butt on 2016-04-02.
-//  Copyright © 2016 Dillon Butt. All rights reserved.
-//
 
 #ifndef MapViewController_h
 #define MapViewController_h
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
-@interface MapViewController : UIViewController <MKMapViewDelegate>
+@interface MapViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate>
 {
     CLLocationCoordinate2D locPark;
     CLLocationCoordinate2D locExam;
     CLLocationCoordinate2D locMe;
+    CLLocation* cl;
+    CLLocationManager* lm;
 }
 
 @property (weak,nonatomic) IBOutlet MKMapView *map;
 
 - (CLLocationCoordinate2D) getCurrentLocation;
+- (void) eventHappened:(NSString*)fname;
 
 @end
 
