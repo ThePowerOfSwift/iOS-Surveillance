574ce5e761626710057903576bc87727519912a4
Dillon <dillonbutt@gmail.com>
2016-04-12 16:03:41 -0230

    Updated for changes

--- Surveillance/MapViewController.m
+++ Surveillance/MapViewController.m
@@ -1,12 +1,6 @@
-//
-//  MapViewController.m
-//  Surveillance
-//
-//  Created by Dillon Butt on 2016-04-02.
-//  Copyright © 2016 Dillon Butt. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
+#import <CFNetwork/CFNetwork.h>
 #import "MapViewController.h"
 
 @implementation MapViewController
@@ -15,74 +9,28 @@
 {
     
     [super viewDidLoad];
+    lm = [[CLLocationManager alloc] init];
+    lm.delegate = self;
+    lm.distanceFilter = kCLDistanceFilterNone;
+    lm.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+    [lm requestAlwaysAuthorization];
+    [lm startUpdatingLocation];
+    
     // Do any additional setup after loading the view, typically from a nib.
-    locPark = CLLocationCoordinate2DMake(47.54745, -52.79422);
-    locExam = CLLocationCoordinate2DMake(47.57490, -52.73529);
     locMe = [self getCurrentLocation];
-    MKCoordinateRegion reg = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(47.56425, -52.70404), 20000, 20000);
+    MKCoordinateRegion reg = MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(47.56425,-52.70404), 20000, 20000);
     self.map.region = reg;
     
     self.map.delegate = self;
-    
-    
-    // Add annotation
-    [self createAnnotation:locPark title:@"iOS Device Name" subtitle:@"Dillon's House"];
-//    MKPointAnnotation* ann = [[MKPointAnnotation alloc] init];
-//    ann.coordinate = locPark;
-//    ann.title = @"Park here";
-//    ann.subtitle = @"Go to exam by 1pm";
-//    [self.map addAnnotation:ann];
-    [self createAnnotation:locExam title:@"iOS Device Name" subtitle:@"School"];
-//    ann = [[MKPointAnnotation alloc] init];
-//    ann.coordinate = locExam;
-//    ann.title = @"4768 exam";
-//    ann.subtitle = @"1-2pm, Mar 21";
-//    [self.map addAnnotation:ann];
-    
-    // Add overlay
-//    CGFloat lat = locPark.latitude;
-//    CLLocationDistance metersPerPoint = MKMetersPerMapPointAtLatitude(lat);
-//    MKMapPoint c = MKMapPointForCoordinate(locPark);
-//    c.x += -43/metersPerPoint;
-//    c.y -= 56/metersPerPoint;
-//    MKMapPoint p1 = MKMapPointMake(c.x, c.y);
-//    p1.y -= 100/metersPerPoint;
-//    MKMapPoint p2 = MKMapPointMake(c.x, c.y);
-//    p2.x += 90/metersPerPoint;
-//    MKMapPoint p3 = MKMapPointMake(c.x, c.y);
-//    p3.x += 190/metersPerPoint;
-//    p3.y -= 90/metersPerPoint;
-//    MKMapPoint p4 = MKMapPointMake(c.x, c.y);
-//    p4.x += 100/metersPerPoint;
-//    p4.y -= 185/metersPerPoint;
-//    MKMapPoint pts[4] = {p1, p2, p3, p4};
-//    MKPolygon* poly = [MKPolygon polygonWithPoints:pts count:4];
-//    [self.map addOverlay:poly];
+    [self getAllCameraEvents];
 }
 
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-//    MKDirectionsRequest *walkingRouteRequest = [[MKDirectionsRequest alloc] init];
-//    walkingRouteRequest.transportType = MKDirectionsTransportTypeWalking;
-//    MKMapItem *startPoint = [[MKMapItem alloc] initWithPlacemark:[[MKPlacemark alloc] initWithCoordinate:locPark addressDictionary:nil]];
-//    MKMapItem *endPoint = [[MKMapItem alloc] initWithPlacemark:[[MKPlacemark alloc] initWithCoordinate:locExam addressDictionary:nil]];
-//    [walkingRouteRequest setSource:startPoint];
-//    [walkingRouteRequest setDestination:endPoint];
-//    
-//    MKDirections *walkingRouteDirections = [[MKDirections alloc] initWithRequest:walkingRouteRequest];
-//    [walkingRouteDirections calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse * walkingRouteResponse, NSError *walkingRouteError)
-//     {
-//         if (walkingRouteError)
-//         {
-//             NSLog(@"Route error: %@", walkingRouteError);
-//         } else
-//         {
-//             [self.map addOverlay:walkingRouteResponse.routes[0].polyline
-//                            level:MKOverlayLevelAboveRoads];
-//         }
-//     }];
+    [self.map removeAnnotations:self.map.annotations];
+    [self getAllCameraEvents];
 }
 
 - (void)didReceiveMemoryWarning
@@ -97,98 +45,203 @@
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
 {
     MKAnnotationView* v = nil;
-    if ([annotation.title isEqualToString:@"Park here"])
+    static NSString* ident = @"pin";
+    v = [mapView dequeueReusableAnnotationViewWithIdentifier:ident];
+    if (v == nil)
     {
-        static NSString* ident = @"pin";
-        v = [mapView dequeueReusableAnnotationViewWithIdentifier:ident];
-        if (v == nil)
-        {
-            v = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:ident];
-            ((MKPinAnnotationView*)v).pinTintColor = [MKPinAnnotationView greenPinColor];
-            v.canShowCallout = YES;
-            ((MKPinAnnotationView *)v).animatesDrop = YES;
-        } else
-        {
-            v.annotation = annotation;
-        }
-    }
-    else if ([annotation.title isEqualToString:@"iOS Device Name"])
+        v = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:ident];
+        v.image = [UIImage imageNamed:@"camera.png"];
+        v.canShowCallout = YES;
+//        v.leftCalloutAccessoryView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"writeicon.png"]];
+//        UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+//        //            [rightButton addTarget:nil action:nil forControlEvents:UIControlEventTouchUpInside];
+//        v.rightCalloutAccessoryView = rightButton;
+    } else
     {
-        static NSString* ident = @"pin";
-        v = [mapView dequeueReusableAnnotationViewWithIdentifier:ident];
-        if (v == nil)
-        {
-            v = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:ident];
-            v.image = [UIImage imageNamed:@"camera.png"];
-            v.canShowCallout = YES;
-            v.leftCalloutAccessoryView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"writeicon.png"]];
-            UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
-            //            [rightButton addTarget:nil action:nil forControlEvents:UIControlEventTouchUpInside];
-            v.rightCalloutAccessoryView = rightButton;
-        } else
-        {
-            v.annotation = annotation;
-        }
+        v.annotation = annotation;
     }
+    
     return v;
-}
-
-- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id <MKOverlay>)overlay
-{
-    MKOverlayRenderer* r = nil;
-    if ([overlay isKindOfClass:[MKPolygon class]]) {
-        MKPolygonRenderer *v = [[MKPolygonRenderer alloc] initWithPolygon:(MKPolygon*)overlay];
-        v.fillColor = [[UIColor redColor] colorWithAlphaComponent:0.1];
-        v.strokeColor = [[UIColor redColor] colorWithAlphaComponent:0.8];
-        v.lineWidth = 2;
-        r = v;
-    }
-    if ([overlay isKindOfClass:[MKPolyline class]]) {
-        MKPolylineRenderer *l = [[MKPolylineRenderer alloc] initWithPolyline:(MKPolyline*)overlay];
-        l.strokeColor = [[UIColor blueColor] colorWithAlphaComponent:0.5];
-        l.lineWidth = 4;
-        r = l;
-    }
-    return r;
 }
 
 - (CLLocationCoordinate2D) getCurrentLocation
 {
-    CLLocationManager *lm = [[CLLocationManager alloc] init];
-    lm.delegate = self;
     lm.desiredAccuracy = kCLLocationAccuracyBest;
     lm.distanceFilter = kCLDistanceFilterNone;
-    CLLocation *loc = [[CLLocation alloc] init];
-    loc = [lm location];
+    cl = [[CLLocation alloc] init];
+    cl = [lm location];
     
     CLLocationCoordinate2D coord;
-    coord.latitude = loc.coordinate.latitude;
-    coord.longitude = loc.coordinate.longitude;
-    if((loc.coordinate.longitude== 0.0 ) && (loc.coordinate.latitude==0.0))
+    
+    NSLog(@"Latitude: %f", lm.location.coordinate.latitude);
+    NSLog(@"Longtitude: %f", lm.location.coordinate.longitude);
+    
+    
+    coord.latitude = cl.coordinate.latitude;
+    coord.longitude = cl.coordinate.longitude;
+    if((cl.coordinate.longitude== 0.0 ) && (cl.coordinate.latitude==0.0))
     {
         NSLog(@"An error has occurred");
         return coord;
     }
     else
     {
-        coord = [loc coordinate];
+        coord = [cl coordinate];
         return coord;
     }
 }
 
-- (void) createAnnotation:(CLLocationCoordinate2D)location title:(NSString*)title subtitle:(NSString*)subtitle
+- (void) createAnnotation:(CLLocationCoordinate2D)location fname:(NSString*)fname uname:(NSString*)uname timestamp:(NSString*)timestamp
 {
     MKPointAnnotation* ann = [[MKPointAnnotation alloc] init];
     ann.coordinate = location;
-    ann.title = title;
-    ann.subtitle = subtitle;
+    NSString* temp = [[fname stringByAppendingString:@" on "] stringByAppendingString:timestamp];
+    ann.title = temp;
+    ann.subtitle = uname;
     [self.map addAnnotation:ann];
 }
 
 - (NSString*) getDeviceName
 {
-    
     return @"Hello world";
 }
+
+- (void) eventHappened:(NSString*)devicename fname:(NSString*)fname
+{
+    NSString *tmpLat = [[NSString alloc] initWithFormat:@"%f", locMe.latitude];
+    NSString *tmpLong = [[NSString alloc] initWithFormat:@"%f", locMe.longitude];
+    
+    NSDate *currentDate = [[NSDate alloc] init];
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"MMMM dd, yyyy, hh:mm aa"];
+    NSString *prettyVersion = [dateFormat stringFromDate:currentDate];
+    NSDate *startdates = [dateFormat dateFromString:prettyVersion];
+    NSLog(@"Timestamp %0.0f",[startdates timeIntervalSince1970]);
+    
+    [self saveEventToDatabase:devicename fname:fname lat:tmpLat longi:tmpLong tstamp:prettyVersion];
+}
+
+- (void) saveEventToDatabase:(NSString*)devicename fname:(NSString*)fname lat:(NSString*)latitude longi:(NSString*)longitude tstamp:(NSString*)timestamp
+{
+    CFStringRef url = CFStringCreateWithFormat(NULL,NULL, CFSTR("http://www.cs.mun.ca/~ddjb25/storeEntry.php?devicename=%@&functionalname=%@&latitude=%@&longitude=%@&timestamps=%@"), devicename,fname,latitude,longitude,timestamp);
+    CFStringRef tmpUrl = CFURLCreateStringByAddingPercentEscapes(NULL, url, NULL, NULL,
+                                            kCFStringEncodingUTF8);
+
+    CFURLRef myURL = CFURLCreateWithString(kCFAllocatorDefault, tmpUrl, NULL);
+    
+    CFStringRef requestMethod = CFSTR("GET");
+    CFHTTPMessageRef myRequest = CFHTTPMessageCreateRequest(kCFAllocatorDefault, requestMethod, myURL, kCFHTTPVersion1_1);
+    
+    CFDataRef mySerializedRequest = CFHTTPMessageCopySerializedMessage(myRequest);
+    
+    CFReadStreamRef requestStream = CFReadStreamCreateForHTTPRequest(NULL, myRequest);
+    
+    CFReadStreamOpen(requestStream);
+    
+    NSMutableData *responseBytes = [NSMutableData data];
+    
+    CFIndex numBytesRead = 0 ;
+    do
+    {
+        UInt8 buf[1024];
+        numBytesRead = CFReadStreamRead(requestStream, buf, sizeof(buf));
+        
+        if(numBytesRead > 0)
+            [responseBytes appendBytes:buf length:numBytesRead];
+        
+    } while(numBytesRead > 0);
+    
+    CFHTTPMessageRef response = (CFHTTPMessageRef)CFReadStreamCopyProperty(requestStream, kCFStreamPropertyHTTPResponseHeader);
+    CFHTTPMessageSetBody(response, (__bridge CFDataRef)responseBytes);
+    NSData *responseBodyData = (NSData*)CFBridgingRelease(CFHTTPMessageCopyBody(response));
+    NSString *responseBody = [[NSString alloc] initWithData:responseBodyData encoding:NSUTF8StringEncoding];
+    if ([responseBody  isEqual: @"success"])
+    {
+        NSLog(@"Storage successful");
+    }
+    else
+    {
+        NSLog(@"Error occurred");
+    }
+    CFReadStreamClose(requestStream);
+    CFRelease(requestStream);
+    
+    CFRelease(myRequest);
+    CFRelease(myURL);
+    CFRelease(url);
+    CFRelease(mySerializedRequest);
+    myRequest = NULL;
+    mySerializedRequest = NULL;
+    
+}
+
+- (void) getAllCameraEvents
+{
+    CFStringRef url = CFSTR("http://www.cs.mun.ca/~ddjb25/getAllCameraEvents.php");
+    CFURLRef myURL = CFURLCreateWithString(kCFAllocatorDefault, url, NULL);
+    
+    CFStringRef requestMethod = CFSTR("GET");
+    CFHTTPMessageRef myRequest = CFHTTPMessageCreateRequest(kCFAllocatorDefault, requestMethod, myURL, kCFHTTPVersion1_1);
+    
+    CFDataRef mySerializedRequest = CFHTTPMessageCopySerializedMessage(myRequest);
+    
+    CFReadStreamRef requestStream = CFReadStreamCreateForHTTPRequest(NULL, myRequest);
+    
+    CFReadStreamOpen(requestStream);
+    
+    NSMutableData *responseBytes = [NSMutableData data];
+    
+    CFIndex numBytesRead = 0 ;
+    do
+    {
+        UInt8 buf[1024];
+        numBytesRead = CFReadStreamRead(requestStream, buf, sizeof(buf));
+        
+        if(numBytesRead > 0)
+            [responseBytes appendBytes:buf length:numBytesRead];
+        
+    } while(numBytesRead > 0);
+    
+    CFHTTPMessageRef response = (CFHTTPMessageRef)CFReadStreamCopyProperty(requestStream, kCFStreamPropertyHTTPResponseHeader);
+    CFHTTPMessageSetBody(response, (__bridge CFDataRef)responseBytes);
+    NSData *responseBodyData = (NSData*)CFBridgingRelease(CFHTTPMessageCopyBody(response));
+    NSString *responseBody = [[NSString alloc] initWithData:responseBodyData encoding:NSUTF8StringEncoding];
+    
+    NSError *error = nil;
+    id object = [NSJSONSerialization
+                 JSONObjectWithData:responseBodyData
+                 options:0
+                 error:&error];
+    for (id foo in object)
+    {
+        float lati = [foo[@"latitude"] floatValue];
+        float longi = [foo[@"longitude"] floatValue];
+        CLLocationCoordinate2D coords = CLLocationCoordinate2DMake(lati, longi);
+        [self createAnnotation:coords fname:foo[@"functionalName"] uname:foo[@"uniqueDeviceName"] timestamp:foo[@"timestamps"]];
+    }
+    
+    CFReadStreamClose(requestStream);
+    CFRelease(requestStream);
+    
+    CFRelease(myRequest);
+    CFRelease(myURL);
+    CFRelease(url);
+    CFRelease(mySerializedRequest);
+    myRequest = NULL;
+    mySerializedRequest = NULL;
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+
+{
+    CLLocation *newLocation = locations.lastObject;
+    
+    CLLocation *loc1 = [[CLLocation alloc] initWithLatitude:locMe.latitude longitude:locMe.longitude];
+    CLLocation *loc2 = [[CLLocation alloc] initWithLatitude:newLocation.coordinate.latitude longitude:newLocation.coordinate.longitude];
+    double distance = [loc1 distanceFromLocation:loc2];
+    locMe = [newLocation coordinate];
+    
+}
+
 
 @end
